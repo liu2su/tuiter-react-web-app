1d4e0fb9adf3dca14aca7d852a2b0299707f6df5
liu2su <liu2su@126.com>
2023-02-24 23:16:34 -0500

    a5

---
+++ public/tuiter/NavigationSidebar/index.js
@@ -0,0 +1,11 @@
+const NavigationSidebar = () => {
+return(`
+    <div class="list-group">
+    <a class="list-group-item" href="/"><i class="fab fa-twitter"></i></a>
+    </div>
+    <div class="d-grid mt-2">
+    <a href="#" class="btn btn-primary btn-block rounded-pill">Tweet</a>
+    </div>`);
+}
+
+export default NavigationSidebar;
